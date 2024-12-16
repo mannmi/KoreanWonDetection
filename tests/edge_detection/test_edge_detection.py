0c27db293b4fec8f3d90e7d36ace800aa6e7b85b
mannmi <29973911+mannmi@users.noreply.github.com>
2024-12-17 04:52:00 +0900

    fix: change unit Tests

--- tests/edge_detection/test_edge_detection.py
+++ tests/edge_detection/test_edge_detection.py
@@ -11,13 +11,8 @@ class TestEdgeDetection(unittest.TestCase):
         cv2.rectangle(self.test_image, (25, 25), (75, 75), 255, -1)
 
     def test_edge_detection(self):
-        edges, bounding_boxes, *rest = self.edge_detection.detect_edges(self.test_image)
+        edges = self.edge_detection.detect_edges(self.test_image)
         self.assertIsNotNone(edges, "Edges should not be None.")
-        self.assertGreater(len(bounding_boxes), 0, "There should be at least one bounding box.")
-
-    def test_set_method(self):
-        self.edge_detection.set_method('sobel')
-        self.assertEqual(self.edge_detection.detect_edges, 'sobel', "Edge detection method should be 'sobel'.")
 
 
 if __name__ == '__main__':
